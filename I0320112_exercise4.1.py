bcda9a34aed1d5cb1c321b9e534822915ee67468
Yolanda Diandari <yolandadiandari@gmail.com>
2021-03-24 17:12:18 +0700

    update tugas 4

--- I0320112_exercise4.1.py
+++ I0320112_exercise4.1.py
@@ -1,1 +1,22 @@
+x = 15
+y = 4
+
+
+#output : x + y = 19
+print('x + y =',x+y)
+
+
+#output : x - y = 11
+print('x - y =',x-y)
+
+#output : x * y = 60
+print('x * y =',x*y)
+
+
+#output : x / y = 3.75
+print(' x / y =',x/y)
+
+
+#output : x ** y = 50625
+print('x ** y =',x**y)
 
