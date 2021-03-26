15f92776dbf32e8cad3dfe98f47847d78cdfc61f
Yolanda Diandari <yolandadiandari@gmail.com>
2021-03-26 07:21:54 +0700

    update tugas 4

--- I0320112_soal6_tugas4.py
+++ I0320112_soal6_tugas4.py
@@ -1,22 +1,22 @@
 x = 4       #4  = 0000 0100
 y = 11      #11 = 0000 1011
 
-#4 | 11     #15 = 0000 1111
+#4 | 11
 a = x|y
 print('nilai 4|11  :',a,'   binary :',format(a,'08b'))
 
-#4 >> 11    #0 = 0000 0000
+#4 >> 11
 a = x >> y
 print('nilai 4>>11 :',a,'    binary :',format(a,'08b'))
 
-#4 ^ 11     #15 = 0000 1111
+#4 ^ 11
 a = x ^ y
 print('nilai 4^11  :',a,'   binary :',format(a,'08b'))
 
-#~4         #-5 = 0000 0101
+#~4
 a = ~x
 print('nilai ~4    :',a,'   binary :',format(a,'08b'))
 
-#11 & 4     #0 = 0000 0000
+#11 & 4
 a = y & x
 print('nilai 4&11  :',a,'    binary :',format(a,'08b'))
